438301862e392ac9a66372fedb957ff4f116926f
Ricardo <rw@pdr.cloud>
2024-10-28 17:10:43 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SmileyComponent } from './smiley/smiley.component';
+import { BasicsComponent } from './basics/basics.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'smiley', component: SmileyComponent },
+  { path: 'basics', component: BasicsComponent },
+  { path: 'app', component: UserListComponent }
+];
