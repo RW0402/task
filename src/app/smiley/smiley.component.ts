a651087e2f9ecb6b12fa921d1e54c6a96e22be50
Ricardo <rw@pdr.cloud>
2024-10-28 17:11:10 +0100

    smiley

---
+++ src/app/smiley/smiley.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smiley',
+  standalone: true,
+  imports: [],
+  templateUrl: './smiley.component.html',
+  styleUrl: './smiley.component.scss'
+})
+export class SmileyComponent {
+
+}
